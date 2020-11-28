3779117b343d66a33c9e81f81e4b2052479ba6bf
David Bell <dtbell99@gmail.com>
2020-11-28 13:15:49 -0500

    Lunchroom updates

--- madlibs.js
+++ madlibs.js
@@ -6,64 +6,54 @@ const generate = (obj) => {
   return result;
 };
 
-const pizza = {
+const lunchroom = {
+  title: 'What happens in the lunchroom, stays in the lunchroom',
+  text: 'Lunchtime in our cafeteria is always <%= question1.answer %>. '
+  + 'They serve hot <%= question2.answer %> and <%= question3.answer %>, but some students <%= question4.answer %> their own <%= question5.answer %> to eat. '
+  + 'Some kids quietly <%= question6.answer %> their <%= question7.answer %>, while others throw <%= question8.answer %> or <%= question9.answer %> when teachers aren\'t looking. ',
   questions: {
-    question1: {
-      type: 'adjective'
-    },
-    question2: {
-      type: 'nationality'
-    },
-    question3: {
-      type: 'person'
-    },
-    question4: {
-      type: 'noun'
-    },
-    question5: {
-      type: 'adjective'
-    },
-    question6: {
-      type: 'noun'
-    },
-    question7: {
-      type: 'adjective'
-    },
-    question8: {
-      type: 'adjective'
-    },
-    question9: {
-      type: 'pluralnoun'
-    },
-    question10: {
-      type: 'noun'
-    },
-    question11: {
-      type: 'number'
-    },
-    question12: {
-      type: 'shapes'
-    },
-    question13: {
-      type: 'food'
-    },
-    question14: {
-      type: 'food'
-    },
-    question15: {
-      type: 'number'
-    }
-  },
+    question1: { type: 'adjective' },
+    question2: { type: 'food' },
+    question3: { type: 'food' },
+    question4: { type: 'verb' },
+    question5: { type: 'nouns' },
+    question6: { type: 'verb' },
+    question7: { type: 'food' },
+    question8: { type: 'vegetables' },
+    question9: { type: 'fruits' }
+  }
+};
+
+const pizza = {
+  title: 'Pizza Pizza',
   text: 'Pizza was invented by a <%= question1.answer %> <%= question2.answer %> chef named <%= question3.answer %>. '
-        + 'To make a pizza, you need to take a lump of <%= question4.answer %>, and make a thin, round <%= question5.answer %> <%= question6.answer %>. '
-        + 'Then you cover it with <%= question7.answer %> sauce, <%= question8.answer %> cheese, and fresh chopped <%= question9.answer %>. '
-        + 'Next you have to bake it in a very hot <%= question10.answer %>. '
-        + 'When it is done, cut it into <%= question11.answer %> <%= question12.answer %>. '
-        + 'Some kids like <%= question13.answer %> pizza the best, but my favorite is the <%= question14.answer %> pizza. '
-        + 'If I could, I would eat pizza <%= question15.answer %> times a day!'
+    + 'To make a pizza, you need to take a lump of <%= question4.answer %>, and make a thin, round <%= question5.answer %> <%= question6.answer %>. '
+    + 'Then you cover it with <%= question7.answer %> sauce, <%= question8.answer %> cheese, and fresh chopped <%= question9.answer %>. '
+    + 'Next you have to bake it in a very hot <%= question10.answer %>. '
+    + 'When it is done, cut it into <%= question11.answer %> <%= question12.answer %>. '
+    + 'Some kids like <%= question13.answer %> pizza the best, but my favorite is the <%= question14.answer %> pizza. '
+    + 'If I could, I would eat pizza <%= question15.answer %> times a day!',
+  questions: {
+    question1: { type: 'adjective' },
+    question2: { type: 'nationality' },
+    question3: { type: 'person' },
+    question4: { type: 'noun' },
+    question5: { type: 'adjective' },
+    question6: { type: 'noun' },
+    question7: { type: 'adjective' },
+    question8: { type: 'adjective' },
+    question9: { type: 'pluralnoun' },
+    question10: { type: 'noun' },
+    question11: { type: 'number' },
+    question12: { type: 'shapes' },
+    question13: { type: 'food' },
+    question14: { type: 'food' },
+    question15: { type: 'number' }
+  }
 };
 
 module.exports = {
   pizza,
+  lunchroom,
   generate
 };
---
+++ tests/madlibs.test.js
@@ -0,0 +1,57 @@
+const assert = require('assert');
+const madlibs = require('../madlibs.js');
+
+describe('madlibs', () => {
+  describe('what happens in the lunchroom', () => {
+    it('should return correct text when generate is called', () => {
+      const lunchroom = { ...madlibs.lunchroom };
+      lunchroom.questions.question1.answer = 'silly';
+      lunchroom.questions.question2.answer = 'biscuits';
+      lunchroom.questions.question3.answer = 'crackers';
+      lunchroom.questions.question4.answer = 'fly';
+      lunchroom.questions.question5.answer = 'garbage';
+      lunchroom.questions.question6.answer = 'sniff';
+      lunchroom.questions.question7.answer = 'cabbage';
+      lunchroom.questions.question8.answer = 'carrots';
+      lunchroom.questions.question9.answer = 'apples';
+
+      const expectedResult = 'Lunchtime in our cafeteria is always silly. '
+      + 'They serve hot biscuits and crackers, but some students fly their own garbage to eat. '
+      + 'Some kids quietly sniff their cabbage, while others throw carrots or apples when teachers aren\'t looking. ';
+      const actualResult = madlibs.generate(lunchroom);
+
+      assert.strictEqual(actualResult, expectedResult);
+    });
+  });
+  describe('pizza pizza', () => {
+    it('should return correct text when generate is called', () => {
+      const pizza = { ...madlibs.pizza };
+      pizza.questions.question1.answer = 'skinny';
+      pizza.questions.question2.answer = 'American';
+      pizza.questions.question3.answer = 'Bob Smith';
+      pizza.questions.question4.answer = 'garbage';
+      pizza.questions.question5.answer = 'smelly';
+      pizza.questions.question6.answer = 'shoe';
+      pizza.questions.question7.answer = 'runny';
+      pizza.questions.question8.answer = 'stinky';
+      pizza.questions.question9.answer = 'french fries';
+      pizza.questions.question10.answer = 'mudpuddle';
+      pizza.questions.question11.answer = '500';
+      pizza.questions.question12.answer = 'squares';
+      pizza.questions.question13.answer = 'turkey';
+      pizza.questions.question14.answer = 'cat food';
+      pizza.questions.question15.answer = '1000';
+
+      const expectedResult = 'Pizza was invented by a skinny American chef named Bob Smith. '
+        + 'To make a pizza, you need to take a lump of garbage, and make a thin, round smelly shoe. '
+        + 'Then you cover it with runny sauce, stinky cheese, and fresh chopped french fries. '
+        + 'Next you have to bake it in a very hot mudpuddle. '
+        + 'When it is done, cut it into 500 squares. '
+        + 'Some kids like turkey pizza the best, but my favorite is the cat food pizza. '
+        + 'If I could, I would eat pizza 1000 times a day!';
+      const actualResult = madlibs.generate(pizza);
+
+      assert.strictEqual(actualResult, expectedResult);
+    });
+  });
+});
